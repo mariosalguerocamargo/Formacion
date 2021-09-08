e4c3428f2bc8fbdf6ac668adfa47fefbc17fb19c
mariosalguerocamargo <53466586+mariosalguerocamargo@users.noreply.github.com>
2021-09-08 09:07:52 +0200

    Add files via upload

---
+++ tipos-de-datos.py
@@ -0,0 +1,31 @@
+import math, random
+
+nada = None
+cadena = "Mario Salguero"
+entero = 99
+flotante = 4.3
+booleano = True
+lista = [1, 54, 23, 124]
+listaString = [1, "Mario", "Salguero", 58, 24]
+tuplaNoCambia = ("Mario", "Salguero", "Master de Python")
+
+
+#Nostrar tipo de dato
+
+print(type(nada))
+print(type(cadena))
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+print(type(lista))
+print(type(listaString))
+print(type(tuplaNoCambia))
+
+print(math.pi)
+print(random.choice([1, 2, 3, 4, 5]))
+
+print(cadena)
+
+cadena = "Nuevo valor"
+
+print(cadena)
---
+++ variables.py
@@ -0,0 +1,27 @@
+"""
+Una variable es un contenedor de informaión que dentro guarda
+"""
+
+texto = "Master de Python"
+texto2 = "Mario Salguero"
+numero = 1
+decimal =6.7
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+numero =77
+decimal =2.5
+
+print(numero)
+print(decimal)
+
+#Concatenación
+
+print(texto + " " + texto2)
+
+print(f"hola, me llamo {texto2} y estoy dando el {texto}")
+
+print("Hola, me llamo {} y estoy {}".format(texto2, texto))
